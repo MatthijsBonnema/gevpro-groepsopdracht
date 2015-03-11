705188ebd972daa907adb5d27a3495b5cfae5112
MatthijsBonnema <m.s.bonnema@student.rug.nl>
2015-03-11 14:17:22 +0100

    Fixed Spawn

--- room_generator.py
+++ room_generator.py
@@ -4,11 +4,29 @@
 # Date: 3/11/15
 # Info: 
 
+# rooms = [((xCor, yCor), item), ((xCor, yCor), item), ((xCor, yCor), item), ..
+#
+
+
 import sys
+from random import randrange
+
+class RoomGenerator():
+    def __init__(self):
+        self.rooms = []
+        self.getrooms()
+
+    def __str__(self):
+        return self.getrooms()
+
+    def getrooms(self):
 
 
-def main():
+        for i in range(20):
+            if randrange(0, 10) <= 1:
+                print(randrange(0,10))
+
 
 
 if __name__ == "__main__":
-    main()
+    RoomGenerator()
