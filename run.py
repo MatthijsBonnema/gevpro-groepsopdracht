ed8f4a88cd29520709380c637bc77ca0ffaaf0e9
MatthijsBonnema <m.s.bonnema@student.rug.nl>
2015-03-11 17:02:42 +0100

    Checks for items nearby

--- run.py
+++ run.py
@@ -26,17 +26,53 @@ def main():
 
 
 
-    print("Welcome {}".format(hunter.getname()))
+    print("Welcome {}\n".format(hunter.getname()))
 
     alive = True
 
     while alive:
-        moveto = input("Please select your move Hunter. up, down, left or right?\n")
+
+        items = []
+
+        # check if something is near
+        xCor, yCor = hunter.getposition()
+        positioncheck = []
+        positioncheck.append((xCor, yCor + 1))
+        positioncheck.append((xCor, yCor - 1))
+        positioncheck.append((xCor + 1, yCor))
+        positioncheck.append((xCor - 1, yCor))
+
+        for coordinates in rooms.showrooms():
+            if coordinates[0] in positioncheck:
+                items.append(coordinates[1])
+        if "gold" in items:
+            print("There is gold near you!")
+        if "bat" in items:
+            print("There is bat near you!")
+        if "pit" in items:
+            print("There is a pit near you!")
+
+        moveto = input("\nPlease select your move Hunter. up, down, left or right?\n")
         move = hunter.move(moveto)
+
         if move == False:
-            print("Not a valid input!")
+            print("Not a valid input!\n")
         else:
-            print("You moved {}!".format(moveto))
+            print("You moved {}!\n".format(moveto))
+            for room in rooms.showrooms():
+                if hunter.getposition() == room[0]:
+                    if room[1] == "pit":
+                        print("You stepped on a {}".format(room[1]))
+                        alive = False
+                    elif room[1] == "gold":
+                        print("You stepped on a {}".format(room[1]))
+                        hunter.foundgold()
+                    elif room[1] == "bat":
+                        print("You stepped on a {}".format(room[1]))
+                        alive = False
+
+    print("You died :O")
+
 
 
 if __name__ == "__main__":
