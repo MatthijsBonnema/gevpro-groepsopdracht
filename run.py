cd39ae0d092a2441f5ae4e2c139eed40749b374a
MatthijsBonnema <m.s.bonnema@student.rug.nl>
2015-03-11 15:35:49 +0100

    Create run.py

---
+++ run.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# File: run.py.py
+# Author: Matthijs Bonnema
+# Date: 3/11/15
+# Info: 
+
+import sys
+
+def main():
+    name = input("What is your name?")
+
+if __name__ == "__main__":
+    main()
